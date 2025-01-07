5df223c5f06ca1256a4c835011c61ad7028bedec
dcapdevila <dcapdevila@opentrends.net>
2025-01-07 23:22:41 +0100

    prod env 2

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/sample-next-app' : '', // Replace <repository-name> with your actual repo name
-  assetPrefix: isProd ? '/sample-next-app' : '', // Replace <repository-name> with your actual repo name
+  basePath: isProd ? '/sample-next-app' : '', 
+  assetPrefix: isProd ? '/sample-next-app' : '', 
   reactStrictMode: true
-  /* config options here */
+  
 };
 
 export default nextConfig;
