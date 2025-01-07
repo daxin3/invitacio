4f74b5ca6567c8440bbe72897c60a5f9216495e5
dcapdevila <dcapdevila@opentrends.net>
2025-01-07 22:12:52 +0100

    new builddeploy.yml files

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/sample-next-app",
+  output: 'export',
+  reactStrictMode: true,
   /* config options here */
 };
 
