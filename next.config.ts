ec35cc37babca53cb9808c94059f942cdd79fb12
dcapdevila <dcapdevila@opentrends.net>
2025-01-08 08:55:31 +0100

    Image src solve

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/sample-next-app', 
-  assetPrefix: 'https://daxin3.github.io/sample-next-app', 
+  // assetPrefix: 'https://daxin3.github.io/sample-next-app', // Seems to not to work.......
   reactStrictMode: true
   
 };
