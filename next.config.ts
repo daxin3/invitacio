5ad9398433276d248da0fd0d869f26e176124dcd
dcapdevila <dcapdevila@opentrends.net>
2025-01-08 07:59:56 +0100

    no-prod variable

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/sample-next-app' : '', 
-  assetPrefix: isProd ? '/sample-next-app' : '', 
+  basePath: '/sample-next-app', 
+  assetPrefix: '/sample-next-app', 
   reactStrictMode: true
   
 };
