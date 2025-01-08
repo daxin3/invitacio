ae47cfb71b54085911906802ce863fc905c8adb9
dcapdevila <dcapdevila@opentrends.net>
2025-01-08 08:16:46 +0100

    assetPrefix: 'https://daxin3.github.io/sample-next-app',

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/sample-next-app', 
-  assetPrefix: '/sample-next-app', 
+  assetPrefix: 'https://daxin3.github.io/sample-next-app', 
   reactStrictMode: true
   
 };
