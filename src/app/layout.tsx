61d161dae4ce8409751daac070172808526a746b
dcapdevila <dcapdevila@opentrends.net>
2025-02-07 23:39:09 +0100

    Etiquetes meta re-colocades

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,22 +25,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (<>
-  
-  <head>
-    <meta property="og:site_name" content="Clàudia & Dani - Invitació" />
-    <meta property="og:title" content="Clàudia & Dani - Invitació" />
-    <meta property="og:description" content="Festa Major" />
-    <meta property="og:image:secure_url" itemProp="image" content="https://daxin3.github.io/invitacio/preview-invitacio.png" />
-    <meta property="og:type" content="website" />
-    <meta property="og:updated_time" content="2025-02-07T21:25:42Z" />
-  </head>
+  return (
     <html lang="es">
-      
+      <head>
+        <meta property="og:site_name" content="Clàudia & Dani - Invitació" />
+        <meta property="og:title" content="Clàudia & Dani - Invitació" />
+        <meta property="og:description" content="Festa Major" />
+        <meta property="og:image:secure_url" itemProp="image" content="https://daxin3.github.io/invitacio/preview-invitacio.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:updated_time" content="2025-02-07T21:25:42Z" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
-    </>
+    
   );
 }
