835df0e402bdf5908c7b4436b9f0333b8e9c5035
dcapdevila <dcapdevila@opentrends.net>
2025-02-08 00:19:43 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="icon" href="/favicon.ico">
-        <link rel="manifest" href="/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="manifest" href="/site.webmanifest"/>
 
         <meta property="og:site_name" content="Clàudia & Dani - Invitació" />
         <meta property="og:title" content="Clàudia & Dani - Invitació" />
