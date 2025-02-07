77a3c4f4d5bbd65dc1e59478b151475236059185
dcapdevila <dcapdevila@opentrends.net>
2025-02-08 00:16:41 +0100

    Favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,12 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="icon" href="/favicon.ico">
+        <link rel="manifest" href="/site.webmanifest">
+
         <meta property="og:site_name" content="Clàudia & Dani - Invitació" />
         <meta property="og:title" content="Clàudia & Dani - Invitació" />
         <meta property="og:description" content="Festa Major" />
@@ -36,10 +42,10 @@ export default function RootLayout({
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="431" />
         <meta property="og:image:height" content="352" />
-        
-        
         <meta property="og:type" content="website" />
         <meta property="og:updated_time" content="2025-02-07T21:25:42Z" />
+
+        
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
