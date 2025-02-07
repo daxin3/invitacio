dc532c98254e655ae759b4a242764e58791ba4a7
dcapdevila <dcapdevila@opentrends.net>
2025-02-08 00:02:57 +0100

    Més meta properties afegits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,13 @@ export default function RootLayout({
         <meta property="og:site_name" content="Clàudia & Dani - Invitació" />
         <meta property="og:title" content="Clàudia & Dani - Invitació" />
         <meta property="og:description" content="Festa Major" />
-        <meta property="og:image:secure_url" itemProp="image" content="https://daxin3.github.io/invitacio/preview-invitacio.png" />
+        <meta property="og:image" content="https://daxin3.github.io/invitacio/preview-invitacio.png" />
+        <meta property="og:image:secure_url" content="https://daxin3.github.io/invitacio/preview-invitacio.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="431" />
+        <meta property="og:image:height" content="352" />
+        
+        
         <meta property="og:type" content="website" />
         <meta property="og:updated_time" content="2025-02-07T21:25:42Z" />
       </head>
